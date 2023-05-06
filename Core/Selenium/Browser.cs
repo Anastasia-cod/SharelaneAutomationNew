d180342d50ccaf1d940a85562ba1f171b86f9a39
admin <nastyabonybelay@gmail.com>
2023-05-06 19:33:03 +0300

    add some part

--- Core/Selenium/Browser.cs
+++ Core/Selenium/Browser.cs
@@ -6,57 +6,57 @@ using OpenQA.Selenium.Firefox;
 
 namespace Core.Selenium
 {
-	public class Browser
-	{
-		private static Browser instance = null;
-		private IWebDriver driver;
+    public class Browser
+    {
+        private static Browser instance = null;
+        private IWebDriver driver;
 
-		public static Browser Instance
-		{
-			get
-			{
-				if(instance == null)
-				{
-					instance = new Browser();
-				}
-				return instance;
-			}
-		}
+        public static Browser Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new Browser();
+                }
+                return instance;
+            }
+        }
 
-		public IWebDriver Driver { get { return driver;  } }
+        public IWebDriver Driver { get { return driver;  } }
 
-		private Browser()
-		{
+        private Browser()
+        {
             switch (TestContext.Parameters.Get("Browser"))
-			{
-				case "chrome":
-			
-					if(bool.Parse(TestContext.Parameters.Get("Headless")!))
-					{
+            {
+                case "chrome":
+            
+                    if(bool.Parse(TestContext.Parameters.Get("Headless")!))
+                    {
                         ChromeOptions options = new ChromeOptions();
                         options.AddArgument("--headless");
                         driver = new ChromeDriver(options);
                     }
-					else
-					{
-						driver = new ChromeDriver();
-					}
+                    else
+                    {
+                        driver = new ChromeDriver();
+                    }
                     
-					break;
-				case "firefox":
-					driver = new FirefoxDriver();
-					break;
-			}
+                    break;
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+            }
 
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
-		public void CloseBrowser()
-		{
-			driver?.Dispose();
-			instance = null;
-		}   
-	}
+        public void CloseBrowser()
+        {
+            driver?.Dispose();
+            instance = null;
+        }   
+    }
 }
 
